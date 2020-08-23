94270034d70761135810b5696aebf7bc4b058f22
mrzepecki <m.rzepaa@gmail.com>
2020-08-23 15:40:55 +0200

    Add MenuAction tests

---
+++ DietDiary.Tests/Services/MenuActionServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using DietDiary.App.Concrete;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DietDiary.Tests.Services
+{
+    public class MenuActionServiceTests
+    {
+        [Fact]
+        public void MenuActionShouldBeNullOrEmpty()
+        {
+            //Arrange
+            MenuActionService actionService = new MenuActionService();
+            //Act
+            var menuAction = actionService.GetMenuActionsByMenuName("nullMenu");
+            //Assert
+            menuAction.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void MenuActionShouldBeNotNull()
+        {
+            //Arrange
+            MenuActionService actionService = new MenuActionService();
+            //Act
+            var menuAction = actionService.GetMenuActionsByMenuName("Main");
+            //Assert
+            menuAction.Count.Should().Be(7);
+        }
+    }
+}
